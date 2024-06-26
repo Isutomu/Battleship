d504bb28d5edc2b3477fe1d5073d6aa6f857f163
Isutomu <isutomu@gmail.com>
2024-06-26 20:57:48 -0300

    feat: and refactor. Modularizes responsibilities.
    Although barebones, the previous iteration of the module was heading in
    a direction in which it was gonna do too many things, so it was dialled
    back to just work as a trigger(setObservers, setBoard) and a really
    simple logic module(changeActivePlayer, endGame).
    The intent is that driver only has to get the ball rolling and help
    in menial tasks, as the data is stored on the gameboards(players) and
    the html interface dictates its interactivity with css's help.

--- src/modules/game-driver.js
+++ src/modules/game-driver.js
@@ -1,17 +1,55 @@
-import Player from './player';
 import createDomBoard from './dom-gameboard';
 
-export function initializeGame() {
-  const player1 = Player('user');
-  const player2 = Player('pc');
+function setBoard(player1, player2) {
+  document.querySelector('.player1-area').innerHTML = '';
+  document.querySelector('.player2-area').innerHTML = '';
 
   //  FOR TEST
   player1.gameboard.addShip(0, 0, 1);
-  console.log(player1.gameboard.ships);
   player2.gameboard.addShip(0, 0, 1);
   //
 
-  const gameArea = document.querySelector('.game');
-  gameArea.appendChild(createDomBoard(player1.gameboard));
-  gameArea.appendChild(createDomBoard(player2.gameboard));
+  document
+    .querySelector('.player1-area')
+    .appendChild(createDomBoard(player1.gameboard));
+  document
+    .querySelector('.player2-area')
+    .appendChild(createDomBoard(player2.gameboard));
+}
+
+function changeActivePlayer(playerNumber) {
+  document
+    .querySelector(`.player${playerNumber}-area`)
+    .setAttribute('disabled', '');
+  document
+    .querySelector(`.player${playerNumber === 1 ? 2 : 1}-area`)
+    .removeAttribute('disabled');
+}
+
+function endGame(players, losingPlayer) {
+  players.forEach((player) => player.observer.disconnect());
+  document.querySelector('.game').setAttribute('disabled', '');
+
+  alert(`game-driver.js endGame, player${losingPlayer === 1 ? 2 : 1} won!`);
+}
+
+function setObservers(players) {
+  const boards = document.querySelectorAll('.board');
+
+  for (let i = 0; i < 2; i++) {
+    players[i].observer = new MutationObserver((mutations, observer) => {
+      changeActivePlayer(i + 1);
+      if (players[i].gameboard.isGameOver()) endGame(players, i + 1);
+    });
+    players[i].observer.observe(boards[i], players[i].observerConfig);
+  }
+}
+
+export default function initializeGame(player1, player2) {
+  document.querySelector('.game').removeAttribute('disabled', '');
+
+  setBoard(player1, player2);
+  document.querySelector('.player2-area').setAttribute('disabled', '');
+  setObservers([player1, player2]);
+  gameSession();
 }

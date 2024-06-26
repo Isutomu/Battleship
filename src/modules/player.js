95561aac63a01e5984dd320e04ca8d7f6f4ecab3
Isutomu <isutomu@gmail.com>
2024-06-26 20:40:16 -0300

    feat: add mutation observe as a property of player
    Player as a ff had to be fine-tuned to its proper role in the app.
    Besides not having even a real reason to exist prior to the changes, the
    mutation observer related properties fit just nice in what you would
    expect from the a "Player" ff in this context, even semantically.

--- src/modules/player.js
+++ src/modules/player.js
@@ -3,6 +3,12 @@ import Gameboard from './gameboard';
 export default function Player(type) {
   let score = 0;
   const gameboard = Gameboard();
+  let observer;
+  const observerConfig = {
+    subtree: true,
+    attributes: true,
+    attributeFilter: ['disabled'],
+  };
 
-  return { type, gameboard };
+  return { type, score, gameboard, observer, observerConfig };
 }

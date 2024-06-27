0006610c23bc7c347d4d13daa7af0713f3a3108d
Isutomu <isutomu@gmail.com>
2024-06-27 14:42:10 -0300

    feat: accommodate 'pc' as possible player.
    For the ai to be properly called into execution refactoring was
    necessary. gameTurn() in special is a new function designed to
    modularize the turns, segregating the responsibility of calling pc ai
    into action as well as when to change the active player.

--- src/modules/game-driver.js
+++ src/modules/game-driver.js
@@ -1,13 +1,11 @@
 import createDomBoard from './dom-gameboard';
+import pcPlays from './pc-ai';
+
+let PC_AI;
 
 function setBoard(player1, player2) {
   document.querySelector('.player1-area').innerHTML = '';
   document.querySelector('.player2-area').innerHTML = '';
-
-  //  FOR TEST
-  player1.gameboard.addShip(0, 0, 1);
-  player2.gameboard.addShip(0, 0, 1);
-  //
 
   document
     .querySelector('.player1-area')
@@ -17,20 +15,48 @@ function setBoard(player1, player2) {
     .appendChild(createDomBoard(player2.gameboard));
 }
 
-function changeActivePlayer(playerNumber) {
+function makePcPlay() {
+  const [x, y] = PC_AI.next().value[0];
+  const player1Squares = document.querySelectorAll(
+    '.player1-area .board .square',
+  );
+
+  player1Squares[10 * x + y].click();
+}
+
+function changeActivePlayer(currentPlayer) {
   document
-    .querySelector(`.player${playerNumber}-area`)
+    .querySelector(`.player${currentPlayer + 1 === 1 ? 2 : 1}-area`)
     .setAttribute('disabled', '');
   document
-    .querySelector(`.player${playerNumber === 1 ? 2 : 1}-area`)
+    .querySelector(`.player${currentPlayer + 1}-area`)
     .removeAttribute('disabled');
 }
 
-function endGame(players, losingPlayer) {
+function endGame(players, winningPlayer) {
   players.forEach((player) => player.observer.disconnect());
   document.querySelector('.game').setAttribute('disabled', '');
 
-  alert(`game-driver.js endGame, player${losingPlayer === 1 ? 2 : 1} won!`);
+  alert(`game-driver.js endGame, player${players[winningPlayer].type} won!`);
+}
+
+function gameTurn(players, playerIndex, hitShip) {
+  const currentPlayer = Math.abs(playerIndex - 1);
+
+  if (players[playerIndex].gameboard.isGameOver()) {
+    endGame(players, currentPlayer);
+  }
+
+  if (hitShip) {
+    if (players[currentPlayer].type === 'pc') {
+      makePcPlay();
+    }
+  } else {
+    changeActivePlayer(currentPlayer);
+    if (players[playerIndex].type === 'pc') {
+      makePcPlay();
+    }
+  }
 }
 
 function setObservers(players) {
@@ -38,8 +64,8 @@ function setObservers(players) {
 
   for (let i = 0; i < 2; i++) {
     players[i].observer = new MutationObserver((mutations, observer) => {
-      changeActivePlayer(i + 1);
-      if (players[i].gameboard.isGameOver()) endGame(players, i + 1);
+      const hitShip = !!mutations[0].target.textContent;
+      gameTurn(players, i, hitShip);
     });
     players[i].observer.observe(boards[i], players[i].observerConfig);
   }
@@ -51,5 +77,9 @@ export default function initializeGame(player1, player2) {
   setBoard(player1, player2);
   document.querySelector('.player2-area').setAttribute('disabled', '');
   setObservers([player1, player2]);
-  gameSession();
+
+  if (player2.type === 'pc') {
+    PC_AI = pcPlays();
+    makePcPlay();
+  }
 }

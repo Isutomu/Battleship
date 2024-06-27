6f92de269dea447cbb30ead8e9f97090658ef9c7
Isutomu <isutomu@gmail.com>
2024-06-27 14:30:35 -0300

    feat: randomly place ships at match start.
    For the auto placement of ships to work, placeShip() had to be improved
    to check if the available requested coordinate was available.
    Two more things had to be determined: what to make of the coordinates
    given (they were determined as the starting point of the ship) and
    how to determine orientation (parameter of the same name receives
    argument with 0 or 1 value, 0 indicating horizontal and 1 vertical).

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,18 +1,47 @@
 import Ship from './ship.js';
 
 export default function Gameboard() {
-  const BOARD_LENGTH = 2;
+  const BOARD_LENGTH = 10;
+  const SHIPS_SIZES = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4];
 
-  const ships = [];
+  const ships = SHIPS_SIZES.map((size) => Ship(size));
   const board = Array(BOARD_LENGTH)
     .fill(0)
     .map(() => Array(BOARD_LENGTH).fill({ hit: false }));
 
-  const addShip = (x, y, length) => {
-    const ship = Ship(length);
+  const placeShip = (x, y, orientation, ship) => {
+    // orientation 0->horizontal/ 1->vertical
+    let checkSpaceFree = true;
+    // return true;
+    if (!orientation) {
+      for (let i = x; i < x + ship.length; i++) {
+        if (board[i][y].ship !== undefined) {
+          checkSpaceFree = false;
+          break;
+        }
+      }
+    } else {
+      for (let i = y; i < y + ship.length; i++) {
+        if (board[x][i].ship !== undefined) {
+          checkSpaceFree = false;
+          break;
+        }
+      }
+    }
 
-    ships.push(ship);
-    board[x][y] = { ship, hit: false };
+    if (checkSpaceFree) {
+      if (!orientation) {
+        for (let i = x; i < x + ship.length; i++) {
+          board[i][y] = { ship, hit: false };
+        }
+      } else {
+        for (let i = y; i < y + ship.length; i++) {
+          board[x][i] = { ship, hit: false };
+        }
+      }
+    }
+
+    return checkSpaceFree;
   };
   const receiveAttack = (x, y) => {
     board[x][y].hit = true;
@@ -20,10 +49,29 @@ export default function Gameboard() {
     if (board[x][y].ship) {
       board[x][y].ship.sunk = true;
       board[x][y].ship.hit();
+      return true;
     }
+    return false;
   };
   const isGameOver = () =>
     ships.reduce((check, ship) => check && ship.getSunk(), true);
 
-  return { ships, board, addShip, receiveAttack, isGameOver };
+  // Randomly placing ships for first execution
+  ships.forEach((ship) => {
+    let checkSuccess = false;
+    do {
+      let positionX = Math.floor(Math.random() * 10);
+      let positionY = Math.floor(Math.random() * 10);
+      const orientation = Math.floor(Math.random() * 2); // 0 horizontal, 1 vertical
+
+      if (!orientation) {
+        positionX = Math.ceil(Math.random() * (10 - ship.length));
+      } else {
+        positionY = Math.ceil(Math.random() * (10 - ship.length));
+      }
+
+      checkSuccess = placeShip(positionX, positionY, orientation, ship);
+    } while (!checkSuccess);
+  });
+  return { ships, board, placeShip, receiveAttack, isGameOver };
 }

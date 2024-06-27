9c5ea8cac2be501e55ab384852c69bcf99208e66
Isutomu <isutomu@gmail.com>
2024-06-27 14:27:01 -0300

    feat: set listener for match start

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './style/reset.css';
 import './style/common.css';
+import './style/gameboard.css';
+
+import initializeMatch from './modules/game-driver';
+import Player from './modules/player';
+
+const player1 = Player('user');
+const player2 = Player('pc');
+
+document
+  .querySelector('.start-game')
+  .addEventListener('click', () => initializeMatch(player1, player2));
